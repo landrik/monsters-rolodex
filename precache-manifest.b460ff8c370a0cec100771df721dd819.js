ea167d35017f2d3972859cf7656193c83b48dc20
Landry Karege <landrik@gmail.com>
2020-04-19 14:16:06 +0100

    Updates

---
+++ precache-manifest.b460ff8c370a0cec100771df721dd819.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0782e72339f68ff53265fe7e2867e5b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "54c12906a67b07c960d1",
+    "url": "/monsters-rolodex/static/css/main.259173a3.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54c12906a67b07c960d1",
+    "url": "/monsters-rolodex/static/js/main.5dc73661.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
